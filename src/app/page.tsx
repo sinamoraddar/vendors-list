676c1d4b24d62c1f378ffda7d1bbc506a01f6951
Sina Moraddar <sinamoraddar@gmail.com>
2023-06-08 18:07:37 +0330

    feat(Packages): install sass and prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,129 @@
-import Image from 'next/image'
+import styles from "./page.module.scss";
+const Card = () => (
+  <div className={styles.card}>
+    <a
+      className="vendorCard"
+      data-id="9616"
+      href="/restaurant/پیتزا_شیلا__پارک_ملت_-r-3x6663?from_list=1&amp;is_pickup=0&amp;item_type=vendor&amp;item_position=vendorList&amp;item_name=پیتزا شیلا (پارک ملت)&amp;item_rank=3&amp;item_code=3x6663"
+    >
+      <section className="_2Ia8D">
+        <header className="_1m0lm">
+          <div className={styles.banner}>
+            <img
+              src="https://cdn.snappfood.ir/400x266/uploads/images/vendors/covers/5e01b8a4bcf22.jpg"
+              alt="پیتزا شیلا (پارک ملت)"
+              className="_1LqfA _3ocXi"
+            />
+          </div>
+          <div>
+            <div className="_2tjy7"></div>
+            <div className="_2lL2p"></div>
+          </div>
+          <div className={styles.logo}>
+            <img
+              src="https://cdn.snappfood.ir/media/cache/vendor_logo/uploads/images/vendors/logos/5e425b1b4cf0b.jpg"
+              alt="پیتزا شیلا (پارک ملت)"
+              className="_3_9MC _3ocXi"
+            />
+          </div>
+        </header>
+        <div className="_1_Jch">
+          <div className="_1Vwjo">
+            <div className="_364L-">
+              <h3 className="_1H36Z">پیتزا شیلا (پارک ملت)</h3>
+              <div className="_1rGaX">
+                <div className="_61kMH _2uVQH" data-is-disable="false">
+                  <span className="_1ERJK">تا</span>
+                  <span>۱۰</span>
+                  <div className="_1B0i4">
+                    <svg
+                      width="7"
+                      height="10"
+                      viewBox="0 0 7 10"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M5.98384 2.49114C6.15147 2.1894 6.04276 1.8089 5.74102 1.64127C5.43928 1.47363 5.05878 1.58235 4.89114 1.88409L1.76614 7.50909C1.59851 7.81083 1.70723 8.19133 2.00897 8.35896C2.3107 8.5266 2.69121 8.41788 2.85884 8.11614L5.98384 2.49114Z"
+                        fill="var(--white-color)"
+                      ></path>
+                      <path
+                        d="M1.68749 2.34386C1.68749 1.91239 2.03727 1.56261 2.46874 1.56261C2.90021 1.56261 3.24999 1.91239 3.24999 2.34386C3.24999 2.77534 2.90021 3.12511 2.46874 3.12511C2.03727 3.12511 1.68749 2.77534 1.68749 2.34386Z"
+                        fill="var(--white-color)"
+                      ></path>
+                      <path
+                        d="M4.49999 7.65636C4.49999 7.22489 4.84977 6.87511 5.28124 6.87511C5.71272 6.87511 6.06249 7.22489 6.06249 7.65636C6.06249 8.08784 5.71272 8.43761 5.28124 8.43761C4.84977 8.43761 4.49999 8.08784 4.49999 7.65636Z"
+                        fill="var(--white-color)"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="_359yZ">
+                <div>
+                  <div>
+                    <div className="_2UpdV">
+                      <span className="DDyzO">
+                        <span className="_1Rd1d">(</span>۳۱۶۴۷
+                        <span className="_1Rd1d">)</span>
+                      </span>
+                      <div className="mJXqQ">
+                        <span className="_1wmAX">۴.۶</span>&nbsp;
+                        <svg
+                          width="12"
+                          height="12"
+                          viewBox="0 0 12 12"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g>
+                            <path
+                              d="M5.55159 0.9086C5.735 0.536977 6.26492 0.536977 6.44833 0.9086L7.76331 3.57306L10.7037 4.00032C11.1138 4.05991 11.2776 4.5639 10.9808 4.85317L8.85313 6.92716L9.35541 9.85568C9.42546 10.2641 8.99675 10.5756 8.62993 10.3828L5.99996 9.00011L3.36998 10.3828C3.00317 10.5756 2.57445 10.2641 2.64451 9.85568L3.14679 6.92716L1.01909 4.85317C0.722336 4.5639 0.88609 4.05991 1.2962 4.00032L4.2366 3.57306L5.55159 0.9086Z"
+                              fill="var(--starcolor-default)"
+                            ></path>
+                            <path
+                              d="M5.55159 0.9086C5.735 0.536977 6.26492 0.536977 6.44833 0.9086L7.76331 3.57306L10.7037 4.00032C11.1138 4.05991 11.2776 4.5639 10.9808 4.85317L8.85313 6.92716L9.35541 9.85568C9.42546 10.2641 8.99675 10.5756 8.62993 10.3828L5.99996 9.00011L3.36998 10.3828C3.00317 10.5756 2.57445 10.2641 2.64451 9.85568L3.14679 6.92716L1.01909 4.85317C0.722336 4.5639 0.88609 4.05991 1.2962 4.00032L4.2366 3.57306L5.55159 0.9086Z"
+                              fill="var(--starcolor-default)"
+                            ></path>
+                          </g>
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="F-KQK">
+            <div className="_2ydF8">
+              فست‌فود&nbsp;&nbsp;پیتزا&nbsp;&nbsp;ساندویچ&nbsp;&nbsp;برگر
+            </div>
+          </div>
+          <div className="e6ueH">
+            <div className="_1Z_vY">
+              <div className="_2JgbG">
+                <span className="_3R-X9">پیک فروشنده </span>
+                <span className="">۹,۶۰۰</span>
+                <span>تومان</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div data-id="9616" data-bid="false"></div>
+      </section>
+    </a>
+  </div>
+);
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      vendors list goes here
+      <div>
+        <Card />
       </div>
     </main>
-  )
+  );
 }
