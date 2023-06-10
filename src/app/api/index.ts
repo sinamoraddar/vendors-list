a809b3db1528c4d150489ca233b13a90b2ef0bf6
Sina Moraddar <sinamoraddar@gmail.com>
2023-06-10 16:33:16 +0330

    fix the pagination issue

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,6 +1,6 @@
 export async function getVendors(page: number) {
   const res = await fetch(
-    `https://snappfood.ir/mobile/v3/restaurant/vendors-list?lat=35.754&long=51.328&page =${page}&page_size=10`
+    `https://snappfood.ir/mobile/v3/restaurant/vendors-list?extra-filter=%7B%22spot%22:+%22list%22%7D&lat=35.774&long=51.418&page=${page}&page_size=10&updateChannels=[%22master%22,%22jek%22,%22twentyPercent%22,%22fiftyPercent%22,%22SeventyPercent%22,%22D1%22,%22transactor%22]&filters=%7B%7D&client=PWA&optionalClient=PWA&deviceType=PWA&appVersion=5.6.6&optionalVersion=5.6.6&UDID=d756c1bd-18bf-43f5-83a7-18546b80cb0a`
   );
 
   // Recommendation: handle errors
